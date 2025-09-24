ee7372d1c66b88ed7082261cacdc4754235a29fb
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-09-25 01:32:01 +0530

    2nd commmit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+import { ConvexReactClient } from "convex/react";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ConvexClientProvider } from "@/convex/provider";
+
 import "./globals.css";
 import { ThemeProvider } from "@/theme/provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -25,10 +29,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+     <ConvexAuthNextjsServerProvider>
+        <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConvexClientProvider >
         <ThemeProvider 
         attribute="class"
         defaultTheme="dark"
@@ -38,8 +44,12 @@ export default function RootLayout({
            {children}
            <Toaster />
            </ThemeProvider>
+           </ConvexClientProvider>
        
       </body>
     </html>
+
+     </ConvexAuthNextjsServerProvider>
+  
   );
 }
